626b1f7697163ede5fc24bcf0330cbf990cf44c0
devendra <devendrakurve094@gmail.com>
2024-09-10 19:40:00 +0530

    dja

---
+++ sept7/numbers_1.py
@@ -0,0 +1,52 @@
+# (decimal to other conversion)
+# x=10
+# print(bin(x))
+# print(oct(x))
+# print(hex(x))
+
+# x=0b1010
+# y=0o157
+# z=0xab
+# x=10
+# print(bin(x))
+# print(oct(x))
+# print(hex(x))
+
+# x=0o123    #(octal to other convertion)
+# print(x)
+# print(hex(x))
+# print(oct(x))
+# print(bin(x))
+
+# x=0xab   #( hexa to other conversion)
+# print(x)
+# print(oct(x))
+# print(bin(x))
+
+# x=0b1010   #( binary to other conversion)
+# print(x)
+# print(oct(x))
+# print(hex(x))
+
+# & and logical operator
+# x=10
+# y=10  #y=20
+# print(x&y)
+
+# '|' or logical operator
+# x=10
+# y=20
+# print(x|y)
+
+# left shit '<<' operator
+# x=10
+# print(x<<1)
+# print(x<<2)
+
+# right shift '>>'
+x=10
+print(x>>1)
+print(x>>2)
+# x=100
+# print(bin(x))
+# print(x>>3)
