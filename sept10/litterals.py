db55eff38e51232422637b802667d67c9ba8d11e
devendra <devendrakurve094@gmail.com>
2024-09-11 19:37:23 +0530

    adj

---
+++ sept10/litterals.py
@@ -0,0 +1,58 @@
+# int 1,2,3,..........
+# x=10
+# print(x)
+# print(type(x))
+# print(id(x))
+
+# float  10.0.........
+# x=10.0
+# print(x)
+# print(type(x))
+# print(id(x))
+
+# complex 'j' last main .............
+# x=10+5j
+# print(x)
+# print(type(x))
+# print(id(x))
+
+
+
+# string== collection of charcters
+# 1 '  ' single line string 
+# 2 "  "  single line string 
+# '"   "' multi line string 
+
+# x='devndra'
+# y="dev"
+# z=''' this id py class'''
+
+# print(x,y,z)
+# print(type(x),type(y),type(z))
+# print(id(x),id(y),id(z))
+
+
+# list :- colllection of homogeneous / hetrogeneous object [ ]
+# my_list1=[10,20,30,40]
+# my_list2=['devndra','dev']
+# my_list3=[10,'devndra',30,'dev']
+
+# print(my_list1,my_list2,my_list3)
+# print(id(my_list1),id(my_list2),id(my_list3))
+# print(type(my_list1),type(my_list2),type(my_list3))
+
+# tuple:- colllection of homogeneous / hetrogeneous object ()
+# my_tuple=(10,'devndra,',30,'dev')
+# print(my_tuple)
+# print(id(my_tuple))
+# print(type(my_tuple))
+
+# discnory :- ('key','value')=keypair   { }
+# duplicate key not allowed
+# duplicate value allowed   
+my_dict={'name':'dev','age':37}
+print(my_dict)
+print(id(my_dict))
+print(type(my_dict))
+
+
