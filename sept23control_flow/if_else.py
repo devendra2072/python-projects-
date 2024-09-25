0b40262befb668a1ef71dadddf784f261a08116c
devendra <devendrakurve094@gmail.com>
2024-09-25 19:38:06 +0530

    25 september

---
+++ sept23control_flow/if_else.py
@@ -0,0 +1,7 @@
+x=int(input("enter age="))
+if x>=18:
+    print("eligible")
+
+else:
+    print("not eligible")
+print("thank")
---
+++ sept23control_flow/ifcondition.py
@@ -0,0 +1,26 @@
+# enter your age:devendra
+# devendra
+# <class 'str'>
+
+# enter your age:[10,20,30] 
+# [10,20,30]
+# <class 'str'>
+
+# enter your age:(10,20,30)
+# (10,20,30)
+# <class 'str'>
+
+# enter your age:{10,20,30}
+# {10,20,30}
+# <class 'str'>
+# x=input("enter your age:")
+# print(x)
+# print(type(x))
+
+x=int(input("enter your age:"))
+# print(x)
+# print(type(x))
+if x>=18:
+    print("welcom to vote",end='.')
+    
+print("thank for visit")    
