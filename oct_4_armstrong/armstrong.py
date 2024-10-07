4359868ddb7e6fcb4d99508f278f39369c179049
devendra <devendrakurve094@gmail.com>
2024-10-07 18:40:08 +0530

    7 octumber

--- oct_4_armstrong/armstrong.py
+++ oct_4_armstrong/armstrong.py
@@ -37,3 +37,28 @@
 # print("reverse digit =",digit)
 
 
+# n=input("enter any value=")
+# x=""
+# for i in n:
+#     x=i+x
+# if n==x:
+#     print("given value is pelindrom")
+# else:
+#     print("given value is not pelindrom")
+
+# ============= febonicci============= 
+n=int(input("enter any value="))
+a=0
+b=1
+i=1
+print(a,end=',')
+print(b,end=',')
+while i<=(n-2):
+    c=a+b
+    a=b
+    b=c
+    if i<n-2:
+        print(c,end=' ')
+    else:
+        print(c)
+    i=i+1
--- octumber_2_tranfer/continue.py
+++ octumber_2_tranfer/continue.py
@@ -84,11 +84,11 @@
 # for i in range(n-1,0,-1):
 #     print("*"*i)
 
-n=int(input("enter any number="))
-for i in range(1,n+1):
-    print(' '*(n-1))
-for i in range(n-1,0,-1):
-    print("*"*i)
+# n=int(input("enter any number="))
+# for i in range(1,n+1):
+#     print(' '*(n-1))
+# for i in range(n-1,0,-1):
+#     print("*"*i)
 
 # *
 # **
@@ -115,17 +115,17 @@ for i in range(n-1,0,-1):
 #      ***
 #      **
 #      *
-# n=int(input("enter any number="))
-# for i in range(1,n+1):
-#     if i==n:
-#         print('*'*i,end='')
-#     else:
-#         print('*'*i)
-# for i in range(n,0,-1):
-#     if i==n:
-#         print(''*(n-i)+'*'*i)
-#     else:
-#         print(' '*n+''*(n-1)+'*'*i)
+n=int(input("enter any number="))
+for i in range(1,n+1):
+    if i==n:
+        print('*'*i,end='')
+    else:
+        print('*'*i)
+for i in range(n,0,-1):
+    if i==n:
+        print(''*(n-i)+'*'*i)
+    else:
+        print(' '*n+''*(n-1)+'*'*i)
     
 
 
